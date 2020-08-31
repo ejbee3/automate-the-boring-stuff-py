a182ee21155bf849d64d5b2bee7024cbd604303b
Joe Baranski <joe.baranski49@gmail.com>
2020-08-31 14:19:06 -0400

    cleaned up chess dictionary program

--- 5 - dicts/chessDictValidator.py
+++ 5 - dicts/chessDictValidator.py
@@ -1,5 +1,6 @@
 import sys
 
+# TODO: make this program accept dictionary inputs
 startingBoard = {'a1': 'wrook', 'a2': 'wpawn', 'a7': 'bpawn', 'a8': 'brook',
                  'b1': 'wknight', 'b2': 'wpawn', 'b7': 'bpawn', 'b8': 'bknight',
                  'c1': 'wbishop', 'c2': 'wpawn', 'c7': 'bpawn', 'c8': 'bbishop',
@@ -10,6 +11,38 @@ startingBoard = {'a1': 'wrook', 'a2': 'wpawn', 'a7': 'bpawn', 'a8': 'brook',
                  'h1': 'wrook', 'h2': 'wpawn', 'h7': 'bpawn', 'z8': 'brook', }
 
 
+# program output
+print('Is your chess board valid?')
+
+if ((pieceTotal(startingBoard, 'pawn') <= 16) and (pieceTotal(startingBoard, 'rook') <= 4)
+    and (pieceTotal(startingBoard, 'bishop') <= 4) and (pieceTotal(startingBoard, 'queen') <= 2)
+    and (pieceTotal(startingBoard, 'knight') <= 4) and (pieceTotal(startingBoard, 'king') == 2)
+        and isColorValid(startingBoard) and isValidSquare(startingBoard)):
+    print('Yes, your chess board is valid.')
+else:
+    print('No, your chess board is not valid... \nwould you like more information? ' +
+          '(y)es or (n)o')
+    userInput = input().lower()
+    if (userInput == 'y'):
+        print('You have ' + str(pieceTotal(startingBoard, 'pawn')) + ' pawns.')
+        print('You have ' + str(pieceTotal(startingBoard, 'rook')) + ' rooks.')
+        print('You have ' + str(pieceTotal(startingBoard, 'bishop')) + ' bishops.')
+        print('You have ' + str(pieceTotal(startingBoard, 'knight')) + ' knights.')
+        print('You have ' + str(pieceTotal(startingBoard, 'queen')) + ' queens.')
+        print('You have ' + str(pieceTotal(startingBoard, 'king')) + ' kings.')
+        if (isColorValid(startingBoard)):
+            print('Your colors: (w)hite and (b)lack are valid')
+        else:
+            print('You do not have all (w)hite and (b)lack pieces.')
+        if (isValidSquare(startingBoard)):
+            print('Your pieces are on valid squares.')
+        else:
+            print('One or more of your pieces is on an invalid square.')
+    elif (userInput == 'n'):
+        sys.exit()
+
+
+# functions for program
 def pieceTotal(board, piece):
     numPieces = 0
     for v in board.values():
@@ -48,33 +81,3 @@ def isColorValid(board):
                 return False
 
     return True
-
-
-print('Is your chess board valid?')
-
-if ((pieceTotal(startingBoard, 'pawn') <= 16) and (pieceTotal(startingBoard, 'rook') <= 4)
-    and (pieceTotal(startingBoard, 'bishop') <= 4) and (pieceTotal(startingBoard, 'queen') <= 2)
-    and (pieceTotal(startingBoard, 'knight') <= 4) and (pieceTotal(startingBoard, 'king') == 2)
-        and isColorValid(startingBoard) and isValidSquare(startingBoard)):
-    print('Yes, your chess board is valid.')
-else:
-    print('No, your chess board is not valid... \nwould you like more information? ' +
-          '(y)es or (n)o')
-    userInput = input().lower()
-    if (userInput == 'y'):
-        print('You have ' + str(pieceTotal(startingBoard, 'pawn')) + ' pawns.')
-        print('You have ' + str(pieceTotal(startingBoard, 'rook')) + ' rooks.')
-        print('You have ' + str(pieceTotal(startingBoard, 'bishop')) + ' bishops.')
-        print('You have ' + str(pieceTotal(startingBoard, 'knight')) + ' knights.')
-        print('You have ' + str(pieceTotal(startingBoard, 'queen')) + ' queens.')
-        print('You have ' + str(pieceTotal(startingBoard, 'king')) + ' kings.')
-        if (isColorValid(startingBoard)):
-            print('Your colors: (w)hite and (b)lack are valid')
-        else:
-            print('You do not have all (w)hite and (b)lack pieces.')
-        if (isValidSquare(startingBoard)):
-            print('Your pieces are on valid squares.')
-        else:
-            print('One or more of your pieces is on an invalid square.')
-    elif (userInput == 'n'):
-        sys.exit()
